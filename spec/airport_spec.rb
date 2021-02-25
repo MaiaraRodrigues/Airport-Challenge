aaed01493ab2d48f6c62deb2effe4591a3329844
Maiara <maiarinhah.eu@gmail.com>
2021-02-25 19:57:06 +0000

    Refactored for more readability

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,38 +5,44 @@ describe Airport do
   let(:plane) { double :plane }
 
   describe '#land' do
-    it 'instructs a plane to land' do
-      allow(airport).to receive(:stormy?).and_return false
-      expect(airport).to respond_to(:land).with(1).argument
-    end
-
-    context 'when full' do
-      it 'does not allow landing' do 
+    context "when not stormy" do
+      before do
         allow(airport).to receive(:stormy?).and_return false
-        30.times do 
-          airport.land(plane)
-        end 
-        expect { airport.land(plane) }.to raise_error "Airport full: Cannot land plane"
+      end 
+
+      it 'instructs a plane to land' do 
+        expect(airport).to respond_to(:land).with(1).argument
       end
+  
+      context 'when full' do
+        it 'does not allow landing' do 
+          30.times do 
+            airport.land(plane)
+          end 
+          expect { airport.land(plane) }.to raise_error "Airport full: Cannot land plane"
+        end
+      end 
     end 
 
+    context 'when stormy' do
       it 'raises an error if asked to land a plane when stormy' do
         allow(airport).to receive(:stormy?).and_return true
         expect { airport.land(plane) }.to raise_error "Cannot land plane: weather is stormy"
       end 
+    end
   end 
+  
+    describe '#take_off' do 
 
-  describe '#take_off' do 
-
-    it 'instructs a plane to take off' do
-      expect(airport).to respond_to(:take_off).with(1).argument
-    end
-    
-    it 'confirms that a plane is no longer at the airport' do
-      airport.land(plane)
-      taking_off = airport.take_off(plane)
-      expect(taking_off).to eq "Plane has taken off"
-    end
+      it 'instructs a plane to take off' do
+        expect(airport).to respond_to(:take_off).with(1).argument
+      end
+      
+      it 'confirms that a plane is no longer at the airport' do
+        airport.land(plane)
+        taking_off = airport.take_off(plane)
+        expect(taking_off).to eq "Plane has taken off"
+      end
   end
 
   context 'defaults' do
